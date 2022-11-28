0b9e22cf076b38885d8aa4e4502f097c1a3d6e04
Snehaittoop <snehaittoop868@gmail.com>
2022-11-28 09:51:44 +0530

    patient monitor added

---
+++ src/main/java/com/example/patientMonitor/PatientMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.patientMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientMonitorApplication.class, args);
+	}
+
+}
